661b505b7a61bea771b6d844cccec4b191dedfc1
Ammar Bdr Kunwar <ammarbdrkunwar58@gmail.com>
2025-04-02 20:55:53 +0545

    day 5 dne

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catlog/pages/home_page.dart';
 import 'package:flutter_catlog/pages/login_page.dart';
+import 'package:flutter_catlog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // home: HomePage(),
       themeMode: ThemeMode.light,
       theme: ThemeData(
@@ -24,8 +26,8 @@ class MyApp extends StatelessWidget {
       // initialRoute: '/home',
       routes: {
         '/': (context) => LoginPage(),
-        '/login': (context) => LoginPage(),
-        '/home': (context) => HomePage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catlog/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,41 +8,49 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset('assets/images/login_image.png', fit: BoxFit.cover),
-          const SizedBox(height: 20),
-          Text(
-            'Welcome',
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 16.0,
-              horizontal: 32.0,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('assets/images/login_image.png', fit: BoxFit.cover),
+            const SizedBox(height: 20),
+            Text(
+              'Welcome',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
-            child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter UserName',
-                    labelText: 'Username',
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16.0,
+                horizontal: 32.0,
+              ),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter UserName',
+                      labelText: 'Username',
+                    ),
                   ),
-                ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Enter Passsword',
-                    labelText: 'Password',
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Enter Passsword',
+                      labelText: 'Password',
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(onPressed: () {}, child: Text('login')),
-              ],
+                  const SizedBox(height: 40),
+                  ElevatedButton(
+                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    onPressed: () {
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    child: Text('login'),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
