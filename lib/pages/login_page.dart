487ccd98807010d1e5983ed4799222d7e921e149
Ammar Bdr Kunwar <ammarbdrkunwar58@gmail.com>
2025-04-02 21:50:38 +0545

    dy6 dne

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -72,7 +72,7 @@ class _LoginPageState extends State<LoginPage> {
                           changeButton
                               ? Icon(Icons.done, color: Colors.white)
                               : Text(
-                                'Login',
+                                'Login ',
                                 style: TextStyle(
                                   fontSize: 18,
                                   color: Colors.white,
