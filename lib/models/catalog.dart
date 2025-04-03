af1522fe30b8cb0f49902ac07e9cee0e16189317
Ammar Bdr Kunwar <ammarbdrkunwar58@gmail.com>
2025-04-03 13:03:30 +0545

    dy 10 dne

---
+++ lib/models/catalog.dart
@@ -0,0 +1,29 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
+
+  final products = [
+    Item(
+      id: 'iphone',
+      name: '14 pro Max',
+      desc: 'BEST phone',
+      price: 1200,
+      color: 'green',
+      image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc',
+    ),
+  ];
+}
